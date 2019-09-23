0c29d3e4afaae04bd063f83c517c9c058d9788b8
Nirha Patel <nrp001@ucsd.edu>
2019-09-23 14:00:17 +0530

    majority elements

---
+++ src/test/java/LeetCode/TestMajorityElement.java
@@ -0,0 +1,14 @@
+package LeetCode;
+
+import org.junit.Test;
+
+public class TestMajorityElement {
+
+  private MajorityElement majorityElement = new MajorityElement();
+
+  @Test
+  public void testMajorityElement() {
+    int nums[] = new int[]{1,1,1,3,3,2,2,2};
+    System.out.println(majorityElement.majorityElement(nums));
+  }
+}
