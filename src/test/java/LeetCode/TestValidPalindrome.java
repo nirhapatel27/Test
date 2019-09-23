2650762b147cecb5bb79b1aeef240a29be8c1f09
Nirha Patel <nrp001@ucsd.edu>
2019-09-23 13:47:55 +0530

    valid palindrome

---
+++ src/test/java/LeetCode/TestValidPalindrome.java
@@ -0,0 +1,15 @@
+package LeetCode;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestValidPalindrome {
+
+  private ValidPalindrome validPalindrome = new ValidPalindrome();
+
+  @Test
+  public void testValidPalindrome() {
+    Assert.assertTrue(validPalindrome.validPalindrome("A man, a plan, a canal: Panama"));
+    Assert.assertFalse(validPalindrome.validPalindrome("aBcdbA"));
+  }
+}
